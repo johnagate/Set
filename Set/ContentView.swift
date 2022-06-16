6e4e5cbcbafcb6b956bd560bf24186e6470401fc
John Agate <johnagate@gmail.com>
2022-06-15 22:20:04 -0700

    initial commit - basic UI added for Set

--- Set/ContentView.swift
+++ Set/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+let cards = Set([0...10])
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Time to play Set!")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }.padding(.horizontal)
+            
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach((1...81), id: \.self) { _ in
+                        CardView().aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }.padding(.horizontal)
+            
+            HStack {
+                Button(action: {}) {
+                    Text("New Game")
+                }
+                Spacer()
+                Button(action: {}) {
+                    Text("Deal Three")
+                }
+            }.padding(10).buttonStyle(.bordered)
+            
+        }
+        
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
     }
 }
 
