33afa237e913f428a791e2b00547caadbdb78a29
John Agate <johnagate@gmail.com>
2022-06-15 21:45:32 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by john agate on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
